aad65565aac18b82d7106e5f2205b2f7ce8e08a7
Austin Wallace <austinwallace196@gmail.com>
2023-04-18 16:28:04 -0400

    updated figure 7

---
+++ plotting/data/t1/h2/README.md
@@ -0,0 +1,5 @@
+data for 
+n=1 : gpu=1
+n=2 : gpu=2
+n=3 : gpu=3
+n=4 : gpu=4
--- plotting/main.py
+++ plotting/main.py
@@ -3,6 +3,63 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+def scrape_output(output_path, ) -> pd.DataFrame:
+    """
+    scrape_output scrapes the output of the program and returns a pandas dataframe
+    """
+    with open(output_path, 'r') as f:
+        lines = f.readlines()
+    atr = {
+        "n": [],
+        "gpus": [],
+        "matrix_ortho": [],
+        "ortho": [],
+        "allreduce": [],
+        "host_device": [],
+        "host_array_allocation": [],
+        "schulz": [],
+        "mem_init": [],
+        "schulz": [],
+        "aTa": [],
+        "MatMul": [],
+    }
+    for l in lines:
+        if l.startswith("procs"):
+            l = l.split("=")
+            n, g = int(l[1].split()[0]), int(l[2])
+            atr["n"].append(n)
+            atr["gpus"].append(g)
+        elif "::ortho" in l:
+            l = l.split('/')
+            atr["ortho"].append(float(l[1]))
+        elif "Timer: matrix_ortho" in l:
+            l = l.split('/')
+            atr["matrix_ortho"].append(float(l[1]))
+        elif "::allreduce" in l:
+            l = l.split('/')
+            atr["allreduce"].append(float(l[1]))
+        elif "::host_device_transfer" in l:
+            l = l.split('/')
+            atr["host_device"].append(float(l[1]))
+        elif "::memory_initialization" in l:
+            l = l.split('/')
+            atr["mem_init"].append(float(l[1]))
+        elif "::host_array_allocation" in l:
+            l = l.split('/')
+            atr["host_array_allocation"].append(float(l[1]))
+        elif "schulz" in l:
+            l = l.split('/')
+            atr["schulz"].append(float(l[1]))
+        elif "::compute_aTa" in l:
+            l = l.split('/')
+            atr["aTa"].append(float(l[1]))
+        elif "::matrix_matrix_multiply" in l:
+            l = l.split('/')
+            atr["MatMul"].append(float(l[1]))
+    df = pd.DataFrame(atr)
+    return df
+
+
 def fig2():
     # Read data
     df = pd.read_csv("data/fig2.csv", header=None, names=["x", "y"])
@@ -85,9 +142,94 @@ def fig7() -> None:
     return
 
 
+def fig7_clusters(
+    data_path="data/t1/h2/ss2.log",
+    cluster="hive",
+    bar_graph_ind=1,
+) -> None:
+    """
+    fig7_ice
+    """
+    df = scrape_output(data_path)
+    fig, ax = plt.subplots(dpi=300)
+    # Pretty tropical colors
+    colors = [
+        # tropical
+        '#FFB300',  # Vivid Yellow
+        '#803E75',  # Strong Purple
+        '#FF6800',  # Vivid Orange
+        '#A6BDD7',  # Very Light Blue
+        '#C10020',  # Vivid Red
+        '#CEA262',  # Grayish Yellow
+        '#817066',  # Medium Gray
+        '#007D34',  # Vivid Green
+        '#F6768E',  # Strong Purplish Pink
+        '#00538A',  # Strong Blue
+    ]
+    labels = [
+        'Total',  # 'matrix_ortho',
+        'Ortho',
+        'AllReduce',
+        'H to D',
+        'H Alloc',
+        'Schulz',
+        'Mem Init',
+        r'$A^TA$',
+        'MatMul',
+    ]
+    cnt = 0
+    for i in df.columns.values:
+        if i == "n" or i == "gpus":
+            continue
+        print(i, cnt)
+        ax.plot(df["n"], df[i], "-o", label=labels[cnt], c=colors[cnt])
+        # ax.plot(df["n"], df[i], "-o", label=i)
+        cnt += 1
+    ax.set_xlabel("MPI Tasks")
+    ax.set_ylabel("Time (s)")
+    ax.set_yscale("log")
+    ax.set_xlim(0.5, 6)
+    ax.set_xticks(range(1, 7))
+    plt.legend()
+    plt.title(cluster.upper())
+    plt.savefig(f"plots/{cluster}_fig7.png")
+    fig, ax = plt.subplots(dpi=300)
+    df2 = df.copy(True)
+    df2.drop(columns=["n"], inplace=True)
+    df2.drop(columns=["gpus"], inplace=True)
+
+    labels = df2.columns.values
+    # print(labels)
+    # labels = [
+    #     'Total',  # 'matrix_ortho',
+    #     'Ortho',
+    #     'AllReduce',
+    #     'H to D',
+    #     'H Alloc',
+    #     'Schulz',
+    #     'Mem Init',
+    #     'A^TA',
+    #     'MatMul',
+    # ]
+    print(labels)
+    values = df2.iloc[bar_graph_ind].values
+    n = df["n"].iloc[bar_graph_ind]
+    g = df["gpus"].iloc[bar_graph_ind]
+    t = f"{cluster.upper()} with np = {n} and ngpu = {g}"
+    ax.bar(labels, values, color=colors)
+    ax.set_xlabel("Tasks")
+    ax.tick_params(axis="x", labelsize=8)
+    ax.set_ylabel("Time (s)")
+    ax.set_yscale("log")
+    plt.title(t)
+    plt.savefig(f"plots/{cluster}_fig7_bar.png")
+    return
+
+
 def main():
-    fig2()
-    fig7()
+    # fig2()
+    # fig7()
+    fig7_clusters()
     return
 
 
