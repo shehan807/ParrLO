bcfe51697af41a956804e42c6da7745fd4a59520
Austin Wallace <awallace43@gatech.edu>
2024-01-29 15:53:50 -0500

    hive tests

---
+++ h2.sh
@@ -0,0 +1,3 @@
+# salloc -A hive-cs207 -phive-gpu -N4 -n48 --mem-per-gpu=32G --gres=gpu:4 -t1:00:00
+# salloc -A hive-cs207 -phive-gpu -N4 -n48 --mem-per-gpu=48G --gres=gpu:4 -t1:00:00 
+salloc -A hive-cs207 -phive-gpu -N2 -n8 --mem=24G --gres=gpu:2 -t1:00:00 
